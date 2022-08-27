b9da95768b532aecdd286e1d9ae686ac77dd00bd
Gee1001 <gjarju99@gmail.com>
2022-08-27 11:26:48 -0700

    implement end points

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3001
 
+
+
 const me ={
     fmane : "Gibril",
     minit : "M",
@@ -15,13 +17,59 @@ const me ={
 
     }
 
+    app.use(express.json())
+    
+    let users = [
+      {
+        id: 2,
+        username: 'Gibril jarju',
+        phon: '7777510',
+        password: 'uhfuieefheg'
+
+
+      },
+
+      {
+        id: 3,
+        username: 'musa ceesay',
+        phon: '9974419',
+        password: 'uhfuieefheg'
+
+      }
+
+
+    ]
+
     app.get('/',(req, res)=>{
         res.send('NAKANGA DEF')
     })
 
-app.get('/me',(req, res)=>{
-    res.json(me)
+app.get('/api/users',(req, res)=>{
+    res.json(users)
 })
+
+app.get('/api/users/:id', (req, res)=>{
+      const id = req.params.id
+      const user = users.find((user) => user.id === Number(id))
+      
+ if(user){
+  res.json (user)
+ }else{res.status(404).end()}
+    })
+
+  app.delete('/api/users/:id', (req, res)=> {
+    const id = req.params.id
+    users = users.filter((user) => user.id !== Number(id))
+    res.status(204).end()
+  })
+
+   app.post('/api/users', (req, res) => {
+    const content = req.body
+
+    console.log(content);
+    res.json(content)
+
+   })
 
 app.listen(port, () =>{
     console.log(`Example app listeening on port ${port}`)
